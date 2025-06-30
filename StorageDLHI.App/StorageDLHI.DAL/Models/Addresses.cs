0c9a7354e20406271b95e59b42cc504f15c489f1
DESKTOP-KD2BPDJ\TUAN DAT <phamtuandat16072001@gmail.com>
2025-06-30 10:42:17 +0700

    Handle Ticket FTWDLHI-5, Update database and create model

---
+++ StorageDLHI.App/StorageDLHI.DAL/Models/Addresses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageDLHI.DAL.Models
+{
+    public class Addresses
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string Ward { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ StorageDLHI.App/StorageDLHI.DAL/Models/Projects.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageDLHI.DAL.Models
+{
+    public class Projects
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string ProjectNo { get; set; }
+        public string ProductInfo { get; set; }
+        public decimal Weight { get; set; }
+
+        public Guid CustomerId { get; set; }
+        public Guid AddressId { get; set; }
+    }
+}
